cf1a933bf51ce34b03afa1a726b9e182bf3df444
Catherine Xie <catherinexie2014@gmail.com>
2025-12-08 09:30:34 -0500

    bug: action type fix

---
+++ frontend/types/Action.ts
@@ -0,0 +1,11 @@
+import { Employee } from './Employee';
+
+export type Action =
+  // | { type: 'edit'; employeeId: number; previousData: Employee }
+  {
+    type: 'delete';
+    employeeId: number;
+    previousData: Employee;
+    index: number;
+    timeoutId: NodeJS.Timeout;
+  };
